afbd8da848ab53630c2235f6e6a59b0e2ac3f96d
raysimen <raysimen@gmail.com>
2025-02-23 19:48:52 -0800

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("today");
-        System.out.println("This is project version 1");
+        System.out.println("today");
+        System.out.println("This is project version 2");
     }
 }
-// done with version one
+// done with version two
