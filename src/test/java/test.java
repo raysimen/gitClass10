158067edd12609768efe33f15251320bd8964c2a
raysimen <raysimen@gmail.com>
2025-02-23 19:06:08 -0800

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("today");
+        System.out.println("This is project version 1");
+    }
+}
+// done with version one
