b73a0ff2488b9a53d5f5e217f62541302262265e
raysimen <raysimen@gmail.com>
2025-02-24 19:17:35 -0800

    person2 commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,7 @@ public class test {
 
     public static void main(String[] args) {
 
-        System.out.println("This is project version 5");
+        System.out.println("This is project version 6 made by person 2");
     }
 }
-// done with version five
+// done with version six
